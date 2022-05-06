fa6c4a62741a7c0faff2efe6fc72fe8626b65396
Abhinav Kumar <abhinavkumar8083@gmail.com>
2022-05-06 21:58:06 +0530

    Api request implemented to fetch data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,23 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-
+  onFormSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+    this.setState({ term: "" });
+  }
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={(event) => this.onFormSubmit(event)}
+        >
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
+              placeholder="Search"
               value={this.state.term}
               onChange={(e) => {
                 this.setState({ term: e.target.value });
